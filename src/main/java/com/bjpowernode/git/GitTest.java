c5232e5cb4a266b090f31b55a2202fd479f83468
hzk <1203201677@qq.com>
2023-04-05 23:38:47 +0800

    first commit!

---
+++ src/main/java/com/bjpowernode/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.git;
+
+/**
+ * @Description:
+ * @Author:HZK
+ * @Date:2023/4/5 23:33
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
